ca7025fd8a2ad5a072247b2d1a7f5b24142068c4
Eduardo Vencovsky (Mac M1 2020) <eduvencovsky@gmail.com>
2024-11-23 17:03:16 +0000

    feat: add adsense script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,19 +42,24 @@ export default function RootLayout({
           href="/favicon/favicon-16x16.png"
         />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
+        {/* <link
           rel="mask-icon"
           href="/favicon/safari-pinned-tab.svg"
           color="#000000"
-        />
+        /> */}
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
+        {/* <meta name="msapplication-TileColor" content="#000000" /> */}
+        {/* <meta
           name="msapplication-config"
           content="/favicon/browserconfig.xml"
-        />
+        /> */}
         <meta name="theme-color" content="#000" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3952419391106561"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body
         className={cn(
